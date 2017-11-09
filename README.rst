f1797d67d3ca077c16560d961972e69f71f287ad
Vlad <lyvladi@gmail.com>
2017-11-09 12:53:57 +0200

    add setup.py to prepare for pypi

---
+++ README.rst
@@ -0,0 +1,7 @@
+PyDCI is a Python implementation of helper classes
+for DCI, Data Context Interaction - A New Role-Based Paradigm for specifying collaborating objects
+originated by Trygve Reenskaug and co-developed by Jim Coplien.
+
+Read more at:
+
+http://fulloo.info/
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='pydci',
+      version='0.0.1',
+      description='PyDCI - Data Context Interaction framework for Python',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords='dci, data, context, interaction, role, roles',
+      url='https://github.com/lygav/pydci',
+      author='Vladimir (Vladi) Lyga',
+      author_email='lyvladi@gmail.com',
+      license='proprietary',
+      packages=['pydci', 'pydci.src'],
+      install_requires=[],
+      test_suite='',
+      tests_require=[],
+      entry_points={},
+      include_package_data=True,
+      zip_safe=False)
